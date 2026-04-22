213ac135acbaadc230dac2a7022e8d91d87d08ff
Ankit yadav <ay870421@gmail.com>
2026-04-22 18:36:01 +0530

    axios file updated for vercel deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api", // backend URL
+  baseURL: "https://skill-brige.vercel.app/api", // backend URL
 });
 
 // attach token automatically
